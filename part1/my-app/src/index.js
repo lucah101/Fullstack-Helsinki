abb45c58bbe4948af3a6a447ab8d3bf31dcdbcfe
lucah101 <56831928+lucah101@users.noreply.github.com>
2024-06-11 22:25:28 +0800

    Update index.js

--- part1/my-app/src/index.js
+++ part1/my-app/src/index.js
@@ -1,3 +1,38 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+
+// let counter = 1
+
+// const refresh = () => {
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App counter={counter} />
+//   )
+// }
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+//   }, 1000)
+
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
